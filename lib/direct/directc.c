d2e8fa6cd8ebd198de6e918c216f670566969d19
Ronny Kramer <ronny.kramer@s2010.tu-chemnitz.de>
2017-04-21 15:45:18 +0200

    directc: split code to improve auto vectorization

--- lib/direct/directc.c
+++ lib/direct/directc.c
@@ -289,65 +289,134 @@ directc_local_one(fcs_int nout, fcs_int nin, fcs_float *xyz, fcs_float *q, fcs_f
   fcs_float dx, dy, dz, ir;
   fcs_float p_sum, f_sum_zero, f_sum_one, f_sum_two;
 
+  // use static, more optimized code if cutoff is 0 (or at least very close to)
+  if (fcs_fabs(cutoff) < 0.000000001) {
 
-  if (fcs_fabs(cutoff) > 0) cutoff = 1.0 / cutoff;
+    // not parallelizable because of access conflicts where inner loop accesses the field of the next outer loop
+    for (i = 0; i < nout; ++i)
+    {
+      p_sum = 0.0;
+      f_sum_zero = 0.0;
+      f_sum_one = 0.0;
+      f_sum_two = 0.0;
+
+      // parallelizable
+#pragma omp parallel for schedule(static) private(j, dx, dy, dz, ir) firstprivate(i, q, xyz, cutoff) reduction(+:p_sum, f_sum_zero, f_sum_one, f_sum_two)
+      for (j = i + 1; j < nout; ++j)
+      {
+        dx = xyz[i * 3 + 0] - xyz[j * 3 + 0];
+        dy = xyz[i * 3 + 1] - xyz[j * 3 + 1];
+        dz = xyz[i * 3 + 2] - xyz[j * 3 + 2];
+
+        ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
+
+        fcs_float temptest = q[j] * ir;
+        p_sum += temptest;
+
+        temptest *= ir * ir;
+        f_sum_zero += temptest * dx;
+        f_sum_one += temptest * dy;
+        f_sum_two += temptest * dz;
+
+        temptest = q[i] * ir;
+        p[j] += temptest;
+
+        temptest *= ir * ir;
+        f[j * 3 + 0] -= temptest * dx;
+        f[j * 3 + 1] -= temptest * dy;
+        f[j * 3 + 2] -= temptest * dz;
+      }
+
+#pragma omp parallel for schedule(static) private(j, dx, dy, dz, ir) firstprivate(i, q, xyz, cutoff) reduction(+:p_sum, f_sum_zero, f_sum_one, f_sum_two)
+      for (j = nout; j < nin; ++j)
+      {
+        dx = xyz[i * 3 + 0] - xyz[j * 3 + 0];
+        dy = xyz[i * 3 + 1] - xyz[j * 3 + 1];
+        dz = xyz[i * 3 + 2] - xyz[j * 3 + 2];
+
+        ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
+
+        fcs_float temptest = q[j] * ir;
+        p_sum += temptest;
+
+        temptest *= ir * ir;
+        f_sum_zero += temptest * dx;
+        f_sum_one += temptest * dy;
+        f_sum_two += temptest * dz;
+      }
+#pragma omp critical
+      {
+        p[i] += p_sum;
+
+        f[i * 3 + 0] += f_sum_zero;
+        f[i * 3 + 1] += f_sum_one;
+        f[i * 3 + 2] += f_sum_two;
+      }
+    }
+  }
+  else
+  {
+    cutoff = 1.0 / cutoff;
 
 // not parallelizable because of access conflicts where inner loop accesses the field of the next outer loop
-  for (i = 0; i < nout; ++i)
-  {
-    p_sum = 0.0;
-    f_sum_zero = 0.0;
-    f_sum_one = 0.0;
-    f_sum_two = 0.0;
+    for (i = 0; i < nout; ++i)
+    {
+      p_sum = 0.0;
+      f_sum_zero = 0.0;
+      f_sum_one = 0.0;
+      f_sum_two = 0.0;
 
 // parallelizable
 #pragma omp parallel for schedule(static) private(j, dx, dy, dz, ir) firstprivate(i, q, xyz, cutoff) reduction(+:p_sum, f_sum_zero, f_sum_one, f_sum_two)
-    for (j = i + 1; j < nout; ++j)
-    {
-      dx = xyz[i*3+0] - xyz[j*3+0];
-      dy = xyz[i*3+1] - xyz[j*3+1];
-      dz = xyz[i*3+2] - xyz[j*3+2];
+      for (j = i + 1; j < nout; ++j)
+      {
+        dx = xyz[i * 3 + 0] - xyz[j * 3 + 0];
+        dy = xyz[i * 3 + 1] - xyz[j * 3 + 1];
+        dz = xyz[i * 3 + 2] - xyz[j * 3 + 2];
 
-      ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
+        ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
 
-      if ((cutoff > 0 && cutoff > ir) || (cutoff < 0 && -cutoff < ir)) continue;
+        if ((cutoff > 0 && cutoff > ir) || (cutoff < 0 && -cutoff < ir))
+          continue;
 
-      p_sum += q[j] * ir;
-      p[j] += q[i] * ir;
+        p_sum += q[j] * ir;
+        p[j] += q[i] * ir;
 
-      f_sum_zero += q[j] * dx * ir * ir * ir;
-      f_sum_one += q[j] * dy * ir * ir * ir;
-      f_sum_two += q[j] * dz * ir * ir * ir;
+        f_sum_zero += q[j] * dx * ir * ir * ir;
+        f_sum_one += q[j] * dy * ir * ir * ir;
+        f_sum_two += q[j] * dz * ir * ir * ir;
 
-      f[j*3+0] -= q[i] * dx * ir * ir * ir;
-      f[j*3+1] -= q[i] * dy * ir * ir * ir;
-      f[j*3+2] -= q[i] * dz * ir * ir * ir;
-    }
+        f[j * 3 + 0] -= q[i] * dx * ir * ir * ir;
+        f[j * 3 + 1] -= q[i] * dy * ir * ir * ir;
+        f[j * 3 + 2] -= q[i] * dz * ir * ir * ir;
+      }
 
 #pragma omp parallel for schedule(static) private(j, dx, dy, dz, ir) firstprivate(i, q, xyz, cutoff) reduction(+:p_sum, f_sum_zero, f_sum_one, f_sum_two)
-    for (j = nout; j < nin; ++j)
-    {
-      dx = xyz[i*3+0] - xyz[j*3+0];
-      dy = xyz[i*3+1] - xyz[j*3+1];
-      dz = xyz[i*3+2] - xyz[j*3+2];
+      for (j = nout; j < nin; ++j)
+      {
+        dx = xyz[i * 3 + 0] - xyz[j * 3 + 0];
+        dy = xyz[i * 3 + 1] - xyz[j * 3 + 1];
+        dz = xyz[i * 3 + 2] - xyz[j * 3 + 2];
 
-      ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
+        ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
 
-      if ((cutoff > 0 && cutoff > ir) || (cutoff < 0 && -cutoff < ir)) continue;
+        if ((cutoff > 0 && cutoff > ir) || (cutoff < 0 && -cutoff < ir))
+          continue;
 
-      p_sum += q[j] * ir;
+        p_sum += q[j] * ir;
 
-      f_sum_zero += q[j] * dx * ir * ir * ir;
-      f_sum_one += q[j] * dy * ir * ir * ir;
-      f_sum_two += q[j] * dz * ir * ir * ir;
-    }
+        f_sum_zero += q[j] * dx * ir * ir * ir;
+        f_sum_one += q[j] * dy * ir * ir * ir;
+        f_sum_two += q[j] * dz * ir * ir * ir;
+      }
 #pragma omp critical
-    {
-      p[i] += p_sum;
+      {
+        p[i] += p_sum;
 
-      f[i*3+0] += f_sum_zero;
-      f[i*3+1] += f_sum_one;
-      f[i*3+2] += f_sum_two;
+        f[i * 3 + 0] += f_sum_zero;
+        f[i * 3 + 1] += f_sum_one;
+        f[i * 3 + 2] += f_sum_two;
+      }
     }
   }
 }
@@ -365,42 +434,83 @@ directc_local_two(fcs_int n0, fcs_float *xyz0, fcs_float *q0, fcs_int n1, fcs_fl
   fcs_float dx, dy, dz, ir;
   fcs_float p_sum, f_sum_zero, f_sum_one, f_sum_two;
 
-
-  if (fcs_fabs(cutoff) > 0) cutoff = 1.0 / cutoff;
-
+  // use static, more optimized code if cutoff is 0 (or at least very close to)
+  if (fcs_fabs(cutoff) < 0.000000001) {
 #pragma omp parallel for schedule(static) private(i, j, dx, dy, dz, ir, p_sum, f_sum_zero, f_sum_one, f_sum_two) firstprivate(q1, xyz0, xyz1, cutoff) shared(p, f)
-  for (i = 0; i < n0; ++i)
-  {
-    p_sum = 0.0;
-    f_sum_zero = 0.0;
-    f_sum_one = 0.0;
-    f_sum_two = 0.0;
+    for (i = 0; i < n0; ++i)
+    {
+      p_sum = 0.0;
+      f_sum_zero = 0.0;
+      f_sum_one = 0.0;
+      f_sum_two = 0.0;
 
 #pragma omp parallel for schedule(static) private(j, dx, dy, dz, ir) reduction(+:p_sum, f_sum_zero, f_sum_one, f_sum_two) firstprivate(xyz0, xyz1, cutoff)
-    for (j = 0; j < n1; ++j)
-    {
-      dx = xyz0[i*3+0] - xyz1[j*3+0];
-      dy = xyz0[i*3+1] - xyz1[j*3+1];
-      dz = xyz0[i*3+2] - xyz1[j*3+2];
+      for (j = 0; j < n1; ++j)
+      {
+        dx = xyz0[i * 3 + 0] - xyz1[j * 3 + 0];
+        dy = xyz0[i * 3 + 1] - xyz1[j * 3 + 1];
+        dz = xyz0[i * 3 + 2] - xyz1[j * 3 + 2];
 
-      ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
+        ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
 
-      if ((cutoff > 0 && cutoff > ir) || (cutoff < 0 && -cutoff < ir)) continue;
+        fcs_float temptest = q1[j] * ir;
+        p_sum += temptest;
 
-      p_sum += q1[j] * ir;
-
-      f_sum_zero += q1[j] * dx * ir * ir * ir;
-      f_sum_one += q1[j] * dy * ir * ir * ir;
-      f_sum_two += q1[j] * dz * ir * ir * ir;
-    }
+        temptest *= ir * ir;
+        f_sum_zero += temptest * dx;
+        f_sum_one += temptest * dy;
+        f_sum_two += temptest * dz;
+      }
 
 #pragma omp critical
-    {
-      p[i] += p_sum;
+      {
+        p[i] += p_sum;
 
-      f[i*3+0] += f_sum_zero;
-      f[i*3+1] += f_sum_one;
-      f[i*3+2] += f_sum_two;
+        f[i * 3 + 0] += f_sum_zero;
+        f[i * 3 + 1] += f_sum_one;
+        f[i * 3 + 2] += f_sum_two;
+      }
+    }
+  }
+  else
+  {
+  cutoff = 1.0 / cutoff;
+
+#pragma omp parallel for schedule(static) private(i, j, dx, dy, dz, ir, p_sum, f_sum_zero, f_sum_one, f_sum_two) firstprivate(q1, xyz0, xyz1, cutoff) shared(p, f)
+    for (i = 0; i < n0; ++i)
+    {
+      p_sum = 0.0;
+      f_sum_zero = 0.0;
+      f_sum_one = 0.0;
+      f_sum_two = 0.0;
+
+#pragma omp parallel for schedule(static) private(j, dx, dy, dz, ir) reduction(+:p_sum, f_sum_zero, f_sum_one, f_sum_two) firstprivate(xyz0, xyz1, cutoff)
+      for (j = 0; j < n1; ++j)
+      {
+        dx = xyz0[i * 3 + 0] - xyz1[j * 3 + 0];
+        dy = xyz0[i * 3 + 1] - xyz1[j * 3 + 1];
+        dz = xyz0[i * 3 + 2] - xyz1[j * 3 + 2];
+
+        ir = 1.0 / fcs_sqrt(z_sqr(dx) + z_sqr(dy) + z_sqr(dz));
+
+        if ((cutoff > 0 && cutoff > ir) || (cutoff < 0 && -cutoff < ir))
+          continue;
+
+        p_sum += q1[j] * ir;
+
+        f_sum_zero += q1[j] * dx * ir * ir * ir;
+        f_sum_one += q1[j] * dy * ir * ir * ir;
+        f_sum_two += q1[j] * dz * ir * ir * ir;
+      }
+
+#pragma omp critical
+      {
+        p[i] += p_sum;
+
+        f[i * 3 + 0] += f_sum_zero;
+        f[i * 3 + 1] += f_sum_one;
+        f[i * 3 + 2] += f_sum_two;
+      }
     }
   }
 }
@@ -489,7 +599,7 @@ directc_local_periodic (fcs_int n0, fcs_float *xyz0, fcs_float *q0, fcs_int n1, 
                             __m512d m512_box_c2_array = _mm512_set1_pd (box_c[2]);
 
                             __m512d m512_q_array = _mm512_set1_pd (q1[j]);
-                            __m512d p_sum_array = _mm512_set1_pd (0.0);
+                            __m512d p_sum_array = _mm512_set1_pd ((double) 0.0);
 
                             for (roundpos = 0; roundpos < roundsize; roundpos += 8)
                               {
